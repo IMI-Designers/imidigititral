d8dc064236316f4b53a99a8c6ebe3814849f666a
IMI-Designers <86943813+IMI-Designers@users.noreply.github.com>
2021-08-02 18:30:32 +0530

    fot

--- footer.php
+++ footer.php
@@ -86,12 +86,12 @@
 <a href="#" class="back-to-top d-flex align-items-center justify-content-center"><i class="bi bi-arrow-up-short"></i></a>
 
 <!-- Vendor JS Files -->
-<script src="<?php bloginfo('template_url'); ?>/assets/vendor/aos/aos.js"></script>
-<script src="<?php bloginfo('template_url'); ?>/assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
-<script src="<?php bloginfo('template_url'); ?>/assets/vendor/glightbox/js/glightbox.min.js"></script>
-<script src="<?php bloginfo('template_url'); ?>/assets/vendor/isotope-layout/isotope.pkgd.min.js"></script>
-<script src="<?php bloginfo('template_url'); ?>/assets/vendor/php-email-form/validate.js"></script>
-<script src="<?php bloginfo('template_url'); ?>/assets/vendor/swiper/swiper-bundle.min.js"></script>
+<script src="<?php echo bloginfo('template_url'); ?>/assets/vendor/aos/aos.js"></script>
+<script src="<?php echo bloginfo('template_url'); ?>/assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+<script src="<?php echo bloginfo('template_url'); ?>/assets/vendor/glightbox/js/glightbox.min.js"></script>
+<script src="<?php echo bloginfo('template_url'); ?>/assets/vendor/isotope-layout/isotope.pkgd.min.js"></script>
+<script src="<?php echo bloginfo('template_url'); ?>/assets/vendor/php-email-form/validate.js"></script>
+<script src="<?php echo bloginfo('template_url'); ?>/assets/vendor/swiper/swiper-bundle.min.js"></script>
 
 <!-- Template Main JS File -->
 <script src="<?php bloginfo('template_url'); ?>/assets/js/main.js"></script>
@@ -120,5 +120,15 @@
 
 </script>
 
+<script>
+function get_bloginfo( $show = '', $filter = 'raw' ) {
+    case 'stylesheet_directory':
+        $output = get_stylesheet_directory_uri();
+        break;
+    case 'template_directory':
+    case 'template_url':
+        $output = get_template_directory_uri();
+        break;
+</script>
 </body>
 </html>
